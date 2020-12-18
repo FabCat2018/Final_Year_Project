af8984335698c0b386548f4e54629f7dbd77a460
Fabio Cataleta <fcataleta@gmail.com>
2020-12-18 16:46:01 +0000

    -Script written to store recommendations for each item so that results can be retrieved faster thus improving performance. -If recommendation system is changed, script must be rerun -Views.py must be modified to make use of script results once system is finalised

---
+++ web_scraper/store_similar_items_script.py
@@ -0,0 +1,35 @@
+from .db_connector import DatabaseConnector
+
+from .recommender_system import find_similar_items_to_target_item
+
+
+def store_similar_items():
+    # Fetch the IDs
+    cursor = DatabaseConnector.setup_db_connection()
+    un_calculated_items_query = """
+        SELECT DISTINCT T1.[item_id] FROM [Final Year Project].[dbo].[Amazon_Video_Games_Ratings_Subset] T1
+        LEFT JOIN [Final Year Project].[dbo].[Item_Id_To_Similar_Item_Ids] T2 ON T2.[target_item] = T1.[item_id]
+        WHERE T2.[target_item] IS NULL
+    """
+    rows = cursor.execute(un_calculated_items_query)
+
+    # Iterate through each of the IDs
+    for target_item in list(rows):
+        similar_items = find_similar_items_to_target_item(target_item.item_id)
+        print("Length of similar items: ", len(similar_items))
+        sql_text = "INSERT INTO [Final Year Project].[dbo].[Item_Id_To_Similar_Item_Ids](target_item, first_similar," \
+                   "second_similar, third_similar, fourth_similar, fifth_similar, sixth_similar, seventh_similar)" \
+                   "VALUES('" + str(target_item.item_id) + "'"
+
+        for element in similar_items:
+            sql_text += ", '" + str(element['item']) + "'"
+
+        while len(similar_items) < 7:
+            sql_text += ", ''"
+            similar_items.append("''")
+
+        sql_text += ");"
+
+        print(sql_text)
+        cursor.execute(sql_text)
+        cursor.commit()
