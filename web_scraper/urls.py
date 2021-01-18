b24663bdc9b5556fbb6df40943ff05923ec0d957
Fabio Cataleta <fcataleta@gmail.com>
2021-01-18 13:23:19 +0000

    -Added id parameter to recommended items list, to allow for searching by id when searching on recommendation -Refactored JavaScript code to be separate methods for later use -Implemented clicking recommendations to run system for specified item

--- web_scraper/urls.py
+++ web_scraper/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/game', views.search_game, name='search_game'),
+    path('search/game/id', views.search_game_by_id, name='search_game_by_id'),
 ]
--- web_scraper/views.py
+++ web_scraper/views.py
@@ -19,8 +19,20 @@ def search_game(request):
         return JsonResponse({"search_results": search_results, "recommender_results": recommender_results})
 
 
+def search_game_by_id(request):
+    if request.is_ajax() and request.method == 'GET':
+        game_id = request.GET.get("game_id", None)
+        search_results = web_scrape(map_id_to_keyword(game_id))
+        recommender_results = _recommend_items_from_id(game_id)
+        return JsonResponse({"search_results": search_results, "recommender_results": recommender_results})
+
+
 def _recommend_items(target_item):
     target_item_id = map_keyword_to_id(target_item)
+    return _recommend_items_from_id(target_item_id)
+
+
+def _recommend_items_from_id(target_item_id):
     similar_item_ids = get_similar_items(target_item_id)
 
     if not similar_item_ids:
@@ -32,6 +44,6 @@ def _recommend_items(target_item):
             key_id = item_id
         else:
             key_id = item_id['item_id']
-        similar_items.append(map_id_to_keyword(key_id))
+        similar_items.append({'name': map_id_to_keyword(key_id), 'id': key_id})
 
     return similar_items
